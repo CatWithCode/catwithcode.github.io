9b3030c4e862a147e82321ce8f6dde7aff03c846
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2025-12-28 02:44:20 +0100

    New entry.

--- Assets/MainFunctions.js
+++ Assets/MainFunctions.js
@@ -33,7 +33,7 @@ const header_STATIC_HTML_INSERT = '<h2 id="Header_Border" align="center">  <a hr
 // ### HARD_CODE_TRIGGER ###
 // footer
 // Assets/BaseFiles/Page/Footer.html
-const footer_STATIC_HTML_INSERT = '<h5 id="Fooder_Border">###DATE_TEXT###&nbsp;&nbsp;|&nbsp;&nbsp;©️ CatWithCode&nbsp;&nbsp;|&nbsp;&nbsp;###LICENSE###<br>&nbsp;&nbsp;Latest Build:<b>2025.12.27 - 03:24</b></h5>';
+const footer_STATIC_HTML_INSERT = '<h5 id="Fooder_Border">###DATE_TEXT###&nbsp;&nbsp;|&nbsp;&nbsp;©️ CatWithCode&nbsp;&nbsp;|&nbsp;&nbsp;###LICENSE###<br>&nbsp;&nbsp;Latest Build:<b>2025.12.28 - 02:43</b></h5>';
 
 // >>> SUB <<<
 // ### HARD_CODE_TRIGGER ###
